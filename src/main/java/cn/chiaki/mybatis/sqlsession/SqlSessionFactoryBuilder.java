91cfce15da50f9a882fc84989acdc2724fa51fbe
chenliang258 <chenliang258@jd.com>
2021-03-12 14:40:39 +0800

    [优化] 优化注释

--- src/main/java/cn/chiaki/mybatis/sqlsession/SqlSessionFactoryBuilder.java
+++ src/main/java/cn/chiaki/mybatis/sqlsession/SqlSessionFactoryBuilder.java
@@ -7,16 +7,15 @@ import cn.chiaki.mybatis.utils.XMLConfigBuilder;
 import java.io.InputStream;
 
 /**
- * SqlSessionFactory工厂类
+ * 会话工厂构建者类
  * @author chenliang258
  * @date 2021-03-13 09:54
  *
- * 用于创建一个SqlSessionFactory对象
  */
 public class SqlSessionFactoryBuilder {
 
     /**
-     * 根据参数的字节输入流构建一个SqlSessionFactory工厂
+     * 根据配置文件输入流构建一个SqlSessionFactory
      * @param in 配置文件的输入流
      * @return SqlSessionFactory
      */
